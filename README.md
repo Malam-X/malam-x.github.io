28de41bdefd39929a049ff9dd097e8c3c4f97ede
DR4G0N5 <63049672+Malam-X@users.noreply.github.com>
2021-02-10 13:48:31 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+
+### Hay Bray
+
+<a href="https://github.com/Malam-X">
+<img align="center" alt="sabesan's Github Stats" src="https://github-readme-stats.codestackr.vercel.app/api?username=Malam-X&show_icons=true&hide_border=true&count_private=true&include_all_commits=true&theme=radical" />
+
+<a href="https://github.com/Malam-X">
+  <img align="center" src="https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=Malam-X&layout=compact&theme=radical" />
+</a>
+
+[![GitHub Malam-X](https://img.shields.io/github/followers/Malam-X?label=follow&style=social)](https://github.com/Malam-X)<br>
+![Malam-X](https://komarev.com/ghpvc/?username=Malam-X&label=Views&color=blue&style=plastic)
+
+[whatsapp]: https://wa.me/+6285731246143
---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+/*******************************************
+        Author   : DR4G0N5
+        Version  : 0.2
+        Team     : Cyber Dakwah Jatim
+        Whatsapp : +62 85731246143
+        "Thanks you!"
+********************************************/
+!(function($) {
+  "use strict";
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+  $(window).on('load', function() {
+    aos_init();
+  });
+})(jQuery);
